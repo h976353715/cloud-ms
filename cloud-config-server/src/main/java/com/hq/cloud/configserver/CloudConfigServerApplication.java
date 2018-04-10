1cf460d28f095ac1d3c017b1dcdde598033e1637
huangqi <976353715@qq.com>
2018-04-10 15:19:31 +0800

    增加配置中心

---
+++ cloud-config-server/src/main/java/com/hq/cloud/configserver/CloudConfigServerApplication.java
@@ -0,0 +1,12 @@
+package com.hq.cloud.configserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CloudConfigServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CloudConfigServerApplication.class, args);
+    }
+}
